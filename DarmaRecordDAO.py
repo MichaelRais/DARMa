1ddaf4b6b5d8590636ed0eff536b35ad88d648bd
Michael Rais <mrais@inbox.com>
2015-05-15 01:53:03 -0400

    Continued work.   Still stubs.

--- DarmaRecordDAO.py
+++ DarmaRecordDAO.py
@@ -1,4 +1,4 @@
-# In Java-land this would be the interface.  As this is Python, it's an abstract class
+# In Java-land this would be the interface.  As this is Python, it's an abstract class since Python support multiple inheritence.
 
 import abc
 
@@ -8,4 +8,22 @@ class DarmaRecordDAO(object):
 
     @abc.abstractmethod
     def get_configuration_info(self):
-        """Method that should do something."""
+        """Method that gets configuration info."""
+        return
+
+    @abc.abstractmethod
+    def add_value_map(self, source, target):
+    	"""Method that adds value map per configuration options"""
+    	return
+
+    @abc.abstractmethod
+    def get_value_map(self, source, target):
+    	"""Method that gets value map(s) per configuration options, if it/they exist(s)"""
+
+    @abc.abstractmethod
+    def del_value_map(self, source, target):
+    	"""Method that deletes value map(s) per configuration options, if it/they exist(s)"""
+
+    @abc.abstractmethod
+    def get_values(self, source):
+    	"""Method that gets all values associated/mapped with/to a source key
--- RecordDaoDataStruct.py
+++ RecordDaoDataStruct.py
@@ -1,22 +1,48 @@
-# This is the concrete class imiplmenting the interface/abstract class
+# This is a/the concrete class implementing the interface/abstract class
 
 import abc
 from DarmaRecordDAO import DarmaRecordDAO
 
 
 class RecordDaoDataStruct(DarmaRecordDAO):
+    def RecordDaoDataStruct(self,Configuration):
+    	"""Constructor that should do something"""
+    	return
+
+    def initialize_record_array(self, controlMode, initRule):
+    	"""Method that should do something."""
+        return
 
     def get_configuration_info_X(self):
         """Method that should do something."""
-        # If abstract method not in concrete class, should throw warning.  
+        # If abstract method not in concrete class, should throw warning.
         # I misnamed _X to trigger warning, but not seeing it.
+        return
 
+    def get_configuration_info(self):
+        """Method that should do something."""
+        return
 
+    def get_range_info(self):
+    	"""Method that should do something."""
+        return
+
+    def add_value_map(self, source, target):
+    	"""Method that should do something."""
+        return
+
+    def get_value_map(self, source, target):
+    	"""Method that should do something."""
+        return
+
+    def del_value_map(self, source, target):
+    	"""Method that should do something."""
+        return
+
+    def get_values(self, source):
+    	"""Method that should do something."""
+        return
 
 if __name__ == '__main__':
-	print('Subclass:', issubclass(RecordDaoDataStruct, DarmaRecordDAO))
-	print ('Instance:', isinstance(RecordDaoDataStruct(), DarmaRecordDAO))
-
-
- 
-
+    print('Subclass:', issubclass(RecordDaoDataStruct, DarmaRecordDAO))
+    print('Instance:', isinstance(RecordDaoDataStruct(), DarmaRecordDAO))
